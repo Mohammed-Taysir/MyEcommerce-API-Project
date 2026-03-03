bbc8b44911fb5ceae4429dff28085fc79457e565
Mohammed Taysir <medo-11223344@hotmail.com>
2026-03-03 16:35:49 +0200

    step 1

---
+++ KASHOP.BLL/Service/CategoryService.cs
@@ -0,0 +1,42 @@
+﻿using KASHOP.DAL.DTO.Request;
+using KASHOP.DAL.DTO.Response;
+using KASHOP.DAL.Models;
+using KASHOP.DAL.Repositry;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.BLL.Service
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepositry _categoryRepositry;
+        public CategoryService(ICategoryRepositry categoryRepositry)
+        {
+            _categoryRepositry = categoryRepositry;
+        }
+        public async Task<CategoryResponse> CreateCategory(CategoryRequest request)
+        {
+            var category = request.Adapt<Category>();
+            await _categoryRepositry.CreateAsync(category);
+
+            return category.Adapt<CategoryResponse>();
+        }
+
+        public async Task<List<CategoryResponse>> GetAllCategories()
+        {
+            var categories = await _categoryRepositry.GetAllAsync(new string[] { nameof(Category.Translations)});
+            return categories.Adapt<List<CategoryResponse>>();
+        }
+
+       public async Task<CategoryResponse?> GetCategory(Expression<Func<Category, bool>> filter)
+        {
+            var category = await _categoryRepositry.GetOne(filter, new string[] { nameof(Category.Translations) });
+            return category.Adapt<CategoryResponse>();
+        }
+    }
+}
---
+++ KASHOP.DAL/Repositry/CategoryRepositry.cs
@@ -0,0 +1,16 @@
+﻿using KASHOP.DAL.Data;
+using KASHOP.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KASHOP.DAL.Repositry
+{
+    public class CategoryRepositry : GenericRepositry<Category>, ICategoryRepositry
+    {
+        public CategoryRepositry(ApplicationDbContext context) : base(context) { }
+    }
+}
